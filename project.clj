f56e8ce0a975094cdff8a74e7fe5b0118233e910
Leena Wooller <leena.wooller@yle.fi>
2014-05-30 08:36:11 +0300

    Project.clj

---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject clojure "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.logging "0.2.6"]
+                 [org.clojure/tools.trace "0.7.6"]
+                 [environ "0.4.0"]]
+  :source-paths ["src"]
+  :jvm-opts ["-Xmx1g"]
+  :test-paths ["test"])
+
+
+
